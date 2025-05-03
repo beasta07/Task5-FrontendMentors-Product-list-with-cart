3f0f12dc5a9fa05d0dc8c0e19deb2c132f8b3691
Pranish Bista <pranish@sysqube.com.np>
2025-05-03 22:51:11 +0545

    Cart Context spelling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import ProductCards from './components/ProductCards'
 import Cart from './components/Cart'
-import { CartProvider } from './context/cartContext'
+import { CartProvider } from './context/CartContext'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <CartProvider >
